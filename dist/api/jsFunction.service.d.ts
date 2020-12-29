791343607b34eccad45a152e1426b64902e04e48
Tauseef <tauseef-ahmad@hotmail.com>
2020-12-30 02:10:46 +0500

    create JS Function

--- dist/api/jsFunction.service.d.ts
+++ dist/api/jsFunction.service.d.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpEvent, HttpHeaders, HttpResponse } from "@angular/commo
 import { PaginationResponse } from "@flexicore/flexicore-client";
 import { Observable } from "rxjs/Observable";
 import { Configuration } from "../configuration";
-import { JSFunctionFilter, JSFunctions } from "../model/models";
+import { JSFunctionCreate, JSFunctionFilter, JSFunctions } from "../model/models";
 export declare class JSFunctionService {
     protected httpClient: HttpClient;
     protected basePath: string;
@@ -13,4 +13,5 @@ export declare class JSFunctionService {
     getAllJSFunctions(body?: JSFunctionFilter, authenticationKey?: string, observe?: 'body', reportProgress?: boolean): Observable<PaginationResponse<JSFunctions>>;
     getAllJSFunctions(body?: JSFunctionFilter, authenticationKey?: string, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<PaginationResponse<JSFunctions>>>;
     getAllJSFunctions(body?: JSFunctionFilter, authenticationKey?: string, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<PaginationResponse<JSFunctions>>>;
+    createJSFunction(body?: JSFunctionCreate, authenticationKey?: string, observe?: any, reportProgress?: boolean): Observable<any>;
 }
--- source/api/jsFunction.service.ts
+++ source/api/jsFunction.service.ts
@@ -3,7 +3,7 @@ import { Inject, Injectable, Optional } from "@angular/core";
 import { BASE_PATH, FlexiCoreDecycle, PaginationResponse } from "@flexicore/flexicore-client";
 import { Observable } from "rxjs/Observable";
 import { Configuration } from "../configuration";
-import { JSFunctionFilter, JSFunctions } from "../model/models";
+import { JSFunctionCreate, JSFunctionFilter, JSFunctions } from "../model/models";
 
 @Injectable()
 export class JSFunctionService 
@@ -66,6 +66,43 @@ export class JSFunctionService
         }
 
         return this.httpClient.post<PaginationResponse<JSFunctions>>(`${this.basePath}/plugins/JSFunction/getAllJSFunctions`,
+            body,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        ).map(o=>FlexiCoreDecycle.retrocycle(o));
+    }
+    public createJSFunction(body?: JSFunctionCreate, authenticationKey?: string, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+
+
+        let headers = this.defaultHeaders;
+        if (authenticationKey !== undefined && authenticationKey !== null) {
+            headers = headers.set('authenticationKey', String(authenticationKey));
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json'
+        ];
+        const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+        if (httpContentTypeSelected != undefined) {
+            headers = headers.set('Content-Type', httpContentTypeSelected);
+        }
+
+        return this.httpClient.post<JSFunctionCreate>(`${this.basePath}/plugins/JSFunction/createJSFunction`,
             body,
             {
                 withCredentials: this.configuration.withCredentials,
